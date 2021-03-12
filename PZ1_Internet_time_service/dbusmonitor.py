55bb21236d046a11b3e5228f9d59e7b82dbfbfb9
Julia-Zhirnova <52165649+Julia-Zhirnova@users.noreply.github.com>
2021-03-12 20:31:27 +0300

    Now close file

--- PZ1_Internet_time_service/dbusmonitor.py
+++ PZ1_Internet_time_service/dbusmonitor.py
@@ -10,11 +10,6 @@ SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 # if we ran script and internet is connected
 START_TIME = time.time()
 
-# open file with flag append (read here https://www.programiz.com/python-programming/methods/built-in/open)
-output_file = open(os.path.join(SCRIPT_PATH, TIME_FILENAME), 'a')
-# creating csv writer for opened file
-csv_writer = csv.writer(output_file)
-
 # now all ready to spawn new process running dbus and parse stdout of first one
 dbus_proc = Popen(["dbus-monitor", "--system",
                    "sender=org.freedesktop.NetworkManager, path=/org/freedesktop/NetworkManager, member=StateChanged"],
@@ -40,10 +35,14 @@ while True:
         START_TIME = time.time()
     # if 70 we should write log to file
     if (conn_code == 20 or conn_code == 40) and START_TIME is not None:
+        # open file with flag append (read here https://www.programiz.com/python-programming/methods/built-in/open)
+        output_file = open(os.path.join(SCRIPT_PATH, TIME_FILENAME), 'a')
+        # creating csv writer for opened file
+        csv_writer = csv.writer(output_file)
         csv_writer.writerow([START_TIME, time.time()])
         START_TIME = None
         # append changes from buffer
         output_file.flush()
+        # don`t forget to close file on exit
+        output_file.close()
 
-# don`t forget to close file on exit
-output_file.close()
