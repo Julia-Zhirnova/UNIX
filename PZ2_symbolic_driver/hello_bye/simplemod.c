146e680b1679b95f17c43e38cbafc7b80714684d
Julia-Zhirnova <52165649+Julia-Zhirnova@users.noreply.github.com>
2021-04-06 23:08:18 +0300

    Update simplemod.c

--- PZ2_symbolic_driver/hello_bye/simplemod.c
+++ PZ2_symbolic_driver/hello_bye/simplemod.c
@@ -12,16 +12,3 @@ static void modtest_exit(void){
 
 module_init(modtest_init);
 module_exit(modtest_exit);
-
-/*
- * usage:
- *  make all
- *  sudo insmod simplemod.ko   # we install module in core
- *  dmesg
- *  --- in the tail u should see Hello !!!
- *  sudo rmmod simplemod   # removes module from kernel
- *  dmesg
- *  --- in the tail u should see Bye !!!
- *  === if u want recompile use ===
- *  make clean
- */
